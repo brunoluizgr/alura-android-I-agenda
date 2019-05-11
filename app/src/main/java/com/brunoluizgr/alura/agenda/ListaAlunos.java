9d911b00822cdf3cedab7c35279736f32eb215ef
Bruno Luiz Gordo Ribeiro <brunoluizgr@gmail.com>
2019-05-11 17:20:51 -0300

    Finishing 'Melhorando a lista'

--- app/src/main/java/com/brunoluizgr/alura/agenda/ListaAlunos.java
+++ app/src/main/java/com/brunoluizgr/alura/agenda/ListaAlunos.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunos extends AppCompatActivity {
 
@@ -18,6 +20,13 @@ public class ListaAlunos extends AppCompatActivity {
         // Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String[] alunos = {"Daniel","Ronald","Jefferson","Rodrigo"};
+        ListView listaAlunos = findViewById(R.id.lvListaAlunos);
+
+        ArrayAdapter<String> arrayAdapterListaAlunos = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(arrayAdapterListaAlunos);
+
     }
 
 }
